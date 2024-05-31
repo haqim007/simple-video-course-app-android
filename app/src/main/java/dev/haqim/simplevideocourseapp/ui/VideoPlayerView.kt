59c1c2f6648c0fc57ecc23f6bea8f2076e7f7f90
haqim007 <khaqim41@yahoo.com>
2024-05-31 07:46:40 +0700

    Update videoplayer fullscreen

--- app/src/main/java/dev/haqim/simplevideocourseapp/ui/VideoPlayerView.kt
+++ app/src/main/java/dev/haqim/simplevideocourseapp/ui/VideoPlayerView.kt
@@ -1,22 +1,27 @@
 package dev.haqim.simplevideocourseapp.ui
 
 import android.net.Uri
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
+@OptIn(UnstableApi::class)
 @Composable
-fun VideoPlayerView(uri: Uri) {
+fun VideoPlayerView(
+    uri: Uri,
+    onFullScreen: (isFullScreen: Boolean) -> Unit = {}
+) {
+
     // Get the current context
     val context = LocalContext.current
 
@@ -46,10 +51,13 @@ fun VideoPlayerView(uri: Uri) {
         factory = { ctx ->
             PlayerView(ctx).apply {
                 player = exoPlayer
+                setFullscreenButtonClickListener {
+                    onFullScreen(it)
+                }
+
             }
         },
         modifier = Modifier
             .fillMaxWidth()
-            .height(300.dp) // Set your desired height
     )
 }
--- app/src/main/java/dev/haqim/simplevideocourseapp/ui/detail/DetailScreen.kt
+++ app/src/main/java/dev/haqim/simplevideocourseapp/ui/detail/DetailScreen.kt
@@ -1,12 +1,20 @@
 package dev.haqim.simplevideocourseapp.ui.detail
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,13 +28,20 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -41,6 +56,9 @@ fun DetailScreen(
     title: String,
     onBackClick: () -> Unit
 ){
+    var isFullScreenState by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     LaunchedEffect(key1 = Unit) {
         viewModel.getCourse(title)
@@ -56,6 +74,15 @@ fun DetailScreen(
             } else {
                 "Unknown Course"
             }
+        }
+    }
+
+    val activity = LocalContext.current as Activity
+    LaunchedEffect(key1 = isFullScreenState) {
+        if (isFullScreenState) {
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        } else {
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
         }
     }
 
@@ -77,67 +104,105 @@ fun DetailScreen(
 
                     data?.let {
                         VideoPlayerView(
-                            Uri.parse(data.videoUrl)
+                            Uri.parse(data.videoUrl),
+                            onFullScreen = {isFullScreen ->
+                                isFullScreenState = isFullScreen
+                            }
                         )
-                        Column(
-                            modifier = Modifier.padding(dimensionResource(id = R.dimen.card_side_margin))
-                        ) {
-                            Text(
-                                text = data.title,
-                                style = MaterialTheme.typography.titleMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 18.sp
-                                )
-                            )
 
-                            Text(
-                                modifier = Modifier.padding(top = 8.dp),
-                                text = "Duration",
-                                style = MaterialTheme.typography.titleMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
+                        if (!isFullScreenState) {
+                            Column(
+                                modifier = Modifier
+                                    .padding(dimensionResource(id = R.dimen.card_side_margin))
+                            ) {
+                                Text(
+                                    text = data.title,
+                                    style = MaterialTheme.typography.titleMedium.copy(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 18.sp
+                                    )
                                 )
-                            )
 
-                            CourseDurationView(data.videoDuration)
+                                Text(
+                                    modifier = Modifier.padding(top = 8.dp),
+                                    text = "Duration",
+                                    style = MaterialTheme.typography.titleMedium.copy(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp
+                                    )
+                                )
 
-                            Text(
-                                modifier = Modifier.padding(top = 8.dp),
-                                text = "Description",
-                                style = MaterialTheme.typography.titleMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
-                                )
-                            )
+                                CourseDurationView(data.videoDuration)
 
-                            Text(
-                                text = data.description,
-                                style = MaterialTheme.typography.bodyMedium .copy(
-                                    fontSize = 14.sp
+                                Text(
+                                    modifier = Modifier.padding(top = 8.dp),
+                                    text = "Description",
+                                    style = MaterialTheme.typography.titleMedium.copy(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp
+                                    )
                                 )
-                            )
 
-                            Text(
-                                modifier = Modifier.padding(top = 8.dp),
-                                text = "Presenter",
-                                style = MaterialTheme.typography.titleMedium.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
+                                Text(
+                                    text = data.description,
+                                    style = MaterialTheme.typography.bodyMedium .copy(
+                                        fontSize = 14.sp
+                                    )
                                 )
-                            )
-                            Text(
-                                text = data.presenterName,
-                                style = MaterialTheme.typography.titleSmall.copy(
-                                    fontSize = 15.sp
+
+                                Text(
+                                    modifier = Modifier.padding(top = 8.dp),
+                                    text = "Presenter",
+                                    style = MaterialTheme.typography.titleMedium.copy(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp
+                                    )
                                 )
-                            )
+                                Text(
+                                    text = data.presenterName,
+                                    style = MaterialTheme.typography.titleSmall.copy(
+                                        fontSize = 15.sp
+                                    )
+                                )
+                            }
                         }
                     } ?: run{
                         Text(text = derivedTitle)
                     }
                 }
             }
-            is Resource.Error -> Unit
+            is Resource.Error -> {
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Log.e("::OnLoadError", uiState.course.message ?: "Error")
+
+                    Text(
+                        modifier = Modifier
+                            .padding(8.dp),
+                        text = uiState.course.message ?: "Error",
+                        textAlign = TextAlign.Center,
+                    )
+
+                    Button(
+                        onClick = {
+                            viewModel.getCourse(title)
+                        },
+                        content = {
+                            Text(text = "Refresh")
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = Color.White,
+                        )
+                    )
+                }
+            }
             else -> {
                 Column(
                     modifier = Modifier
@@ -193,5 +258,14 @@ private fun DetailTopAppBar(
             }
         },
         modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun CourseListItemViewPreview() {
+    DetailScreen(
+        title = "G12 Chemistry",
+        onBackClick = {}
     )
 }
